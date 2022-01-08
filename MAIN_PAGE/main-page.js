e60db6dcba8d6273017d1feb90ad8dced05f1bda
MACXDON <72674976+MACXDON@users.noreply.github.com>
2022-01-07 19:16:03 -0600

    "Responsive menu and social links added"

--- MAIN_PAGE/main-page.js
+++ MAIN_PAGE/main-page.js
@@ -1,29 +1,3 @@
-let aboutMeButton = document.querySelector('.about-me');
-let contactButton = document.querySelector('.contact');
-let projectsButton = document.querySelector('.projects');
-
-let content = document.querySelector('.content');
-let contactPage = document.querySelector('.contact-page');
-let projectPage = document.querySelector('.project-page');
-
-aboutMeButton.addEventListener('click', () => {
-    content.style.display = 'block';
-    projectPage.style.display = 'none';
-    contactPage.style.display = 'none';
-})
-
-contactButton.addEventListener('click', () => {
-    content.style.display = 'none';
-    projectPage.style.display = 'none';
-    contactPage.style.display = 'block';
-})
-
-projectsButton.addEventListener('click', () => {
-    content.style.display = 'none';
-    contactPage.style.display = 'none';
-    projectPage.style.display = 'block';
-})
-
 let menuButton = document.querySelector('.menu-button');
 
 menuButton.addEventListener('click', () => {
