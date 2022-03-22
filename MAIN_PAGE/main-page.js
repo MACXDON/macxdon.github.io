c8baa9bc3980ff128326ee2e9f7573aac11774cf
MACXDON <72674976+MACXDON@users.noreply.github.com>
2022-03-22 15:40:22 -0500

    Email form update

--- MAIN_PAGE/main-page.js
+++ MAIN_PAGE/main-page.js
@@ -9,3 +9,20 @@ menuButton.addEventListener('click', () => {
         navList.style.display = 'none';
     }
 })
+
+let emailDisplay = document.querySelector('.email-display');
+let emailExit = document.querySelector('.exit-email-form');
+
+const emailFormDisplayToggle = () => {
+    let emailForm = document.querySelector('#email-submission-form');
+
+    if(emailForm.style.display === 'none') {
+        emailForm.style.display = 'block';
+    } else {
+        emailForm.style.display = 'none'
+    }
+}
+
+emailDisplay.addEventListener('click', emailFormDisplayToggle)
+
+emailExit.addEventListener('click', emailFormDisplayToggle)
